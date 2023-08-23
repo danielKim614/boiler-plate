820f83cb477c6a92c64de077d628a85a848b8c0e
Seungki Kim <ksg980614@gmail.com>
2023-08-23 23:31:28 +0900

    [ADD] Logout function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 // application/x-www-form-urlencoded
@@ -22,7 +22,7 @@ mongoose
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     // 회원가입할 때 필요한 정보들을 client에서 가져오면
     // 그것들을 데이터베이스에 넣어준다.
 
@@ -40,10 +40,10 @@ app.post('/register', (req, res) => {
     // });
 });
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
     // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
-    User.findOne({ email: req.body.email }
+    User.findOne({ email: req.body.email })
         .then(docs => {
             if(!docs) {
                 return res.json({
@@ -70,8 +70,8 @@ app.post('/login', (req, res) => {
         })
         .catch(err => {
             return res.status(400).send(err);
-        })
-    );
+        });
+});
 
     // // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
     // User.findOne({ email: req.body.email }, (err, userInfo) => {
@@ -98,6 +98,36 @@ app.post('/login', (req, res) => {
     //         })
     //     })
     // });
+
+app.get('/api/users/auth', auth, (req, res) => {
+    
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    });
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({_id: req.user._id}, { token: "" })
+        .then((user) => {
+            res.status(200).send({ success: true })
+        })
+        .catch((err) => {
+            res.json({ success: false, err });
+        });
+    // User.findOneAndUpdate({_id: req.user._id}, { token: "" }, (err, user) => {
+    //     if(err) return res.json({ success: false, err });
+    //     return res.status(200).send({
+    //         success: true
+    //     });
+    // })
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
